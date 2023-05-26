2f306b4cd3a6fd8ddd0750b68f0954a04efd6d43
Shaaamvs <130343285+Shaaamvs@users.noreply.github.com>
2023-05-26 16:53:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import Navigation from './Component/Navigation'
+import Banner from './Component/Banner';
+import './App.css';
+import Singlebag from './Component/Singlebag';
+
+function App() {
+  return (
+
+<>
+{/* <h1 className='name'>Welcome</h1> */}
+
+<Navigation/>
+<Banner/>
+<Singlebag/>
+</>
+
+
+  );
+}
+
+export default App;
